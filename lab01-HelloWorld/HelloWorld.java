605e249d92f0e1b4d2ac662c0b966005aafa9ae5
aps219 <aps219@lehigh.edu>
2016-01-29 16:50:14 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+//////////////////////////////////////
+//Aidan Spencer
+//Hello World Java Program
+//Program description = Hello World Message
+
+//  define class
+public class HelloWorld{
+    
+//  add main method
+  public static void main(String[] args) {
+      
+//  print statement with border
+    System.out.println("##########################");
+    System.out.println("##########################");
+    System.out.println("#                        #");
+    System.out.println("#   Hello World 2016!!   #");
+    System.out.println("#                        #");
+    System.out.println("##########################");
+    System.out.println("##########################");
+
+  }
+}
